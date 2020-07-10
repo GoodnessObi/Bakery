3aa6e0be8e34ea1c9c04cf7585135658ed0fe556
goodness obi <goodnessobi@gmail.com>
2020-07-10 17:52:37 +0100

    feature: add slider to services  section

---
+++ assets/script/script.js
@@ -0,0 +1,41 @@
+//Services Image Slider
+$('.slick-slider').slick({
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll: 4,
+	arrows: false,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	dots: true,
+	draggable: true,
+	pauseOnHover: true,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+			},
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				infinite: true,
+			},
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+			},
+		},
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	],
+});
